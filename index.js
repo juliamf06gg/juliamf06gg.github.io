ed36f5166ae7a9853306ded562124ebdb8ce1a5b
juliamf06gg <plataformaspagantesgg@gmail.com>
2024-09-29 00:19:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const telefonesInput = document.getElementById('telefones');
 const botao = document.getElementById('formatarBtn');
 const resultadoDiv = document.getElementById('resultado');
+const copiarBtn = document.getElementById('copiarBtn');
 
 function formatarTelefone(telefone) {
     let novoTelefone = '';
     for (let i = 0; i < telefone.length; i++) {
         if (telefone[i] !== ' ' && telefone[i] !== '-' && telefone[i] !== '+') {
-            novoTelefone += `${telefone[i]}`;
+            novoTelefone += telefone[i];
         }
     }
     return novoTelefone;
@@ -16,13 +17,26 @@ botao.onclick = function () {
     // Obtém a lista de telefones e separa por linha
     const telefones = telefonesInput.value.split('\n');
 
-    
     // Limpa o conteúdo anterior
     resultadoDiv.innerHTML = '';
 
     // Formata cada telefone e exibe no div de resultado
     telefones.forEach(telefone => {
         const telefoneFormatado = formatarTelefone(telefone);
-        resultadoDiv.innerHTML += `${telefoneFormatado}`;
+        resultadoDiv.innerHTML += `${telefoneFormatado}\n`;
     });
+};
+
+copiarBtn.onclick = function () {
+    const textToCopy = resultadoDiv.innerText;
+
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(textToCopy).then(() => {
+        alert('Copiou')
+        }).catch(err => {
+            console.log(err)
+        });
+    } else {
+        console.log('Erro')
+    }
 };
